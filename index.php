d5cba7512b67d3a35a3a2f5f1f2ff68eafc8f51b
Scott Chen <scott@oopgo.com>
2012-11-01 01:16:56 -0700

    init

---
+++ index.php
@@ -0,0 +1,15 @@
+<div class="container">
+    <div class="contr"><h2>Drag and Drop your images to 'Drop Area' (up to 5 files at a time, size - under 256kb)</h2></div>
+    <div class="upload_form_cont">
+        <div id="dropArea">Drop Area</div>
+
+        <div class="info">
+            <div>Files left: <span id="count">0</span></div>
+            <div>Destination url: <input id="url" value="http://www.script-tutorials.com/demos/257/upload.php"/></div>
+            <h2>Result:</h2>
+            <div id="result"></div>
+            <canvas width="500" height="20"></canvas>
+        </div>
+    </div>
+</div>
+<script src="js/script.js"></script>
