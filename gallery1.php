a0d15a3fc1fdfb24832f824d4f8d054a08522e14
Scott Chen <scott@oopgo.com>
2012-11-02 23:30:14 -0700

    renamed index to gallery1

---
+++ gallery1.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+    <link rel="stylesheet" href="css/main.css">
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+   
+</head>
+<body>
+    <div id="dropArea">
+        <div id="gallery_instruction">
+        <!--
+            <a href="#" id="to_square">to square</a><br>
+            <a href="#" id="to_rect">to rect</a>
+        -->
+        </div>
+    </div>
+    <script src="js/gallery.js"></script>
+    <script src="js/gallery_tools.js"></script>
+    <script src="js/jquery.wookmark.js"></script>
+</body>
+</html>
