31e2248bd27048a35d33fd113efd2f4c9b18773c
Scott Chen <scott@oopgo.com>
2012-11-02 23:20:05 -0700

    updated gallery function

--- index.php
+++ index.php
@@ -7,12 +7,14 @@
 <body>
     <div id="dropArea">
         <div id="gallery_instruction">
-            drag files and drop in the gray area<br>
+        <!--
             <a href="#" id="to_square">to square</a><br>
             <a href="#" id="to_rect">to rect</a>
+        -->
         </div>
     </div>
-    <script src="js/script.js"></script>
+    <script src="js/gallery.js"></script>
     <script src="js/gallery_tools.js"></script>
+    <script src="js/jquery.wookmark.js"></script>
 </body>
 </html>
