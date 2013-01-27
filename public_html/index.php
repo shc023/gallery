557f769c2dc34c0e604f9f8e99e4ba3b88b5d2bf
Chris Bednarski <banzaimonkey@gmail.com>
2013-01-26 20:17:02 -0800

    Added a placeholder index controller

--- public_html/index.php
+++ public_html/index.php
@@ -3,6 +3,10 @@ require '../vendor/autoload.php';
 
 $app = new \Slim\Slim();
 
+$app->get('/', function() {
+	echo 'This is the index';
+});
+
 $app->get('/hello/:name', function ($name) {
     echo "Hello, $name";
 });
