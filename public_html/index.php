f29b62351648e65fbd9063aaab9cba17ced70b34
Scott Chen <schen@riotgames.com>
2013-01-26 18:57:24 -0800

    adding testing stuff for slim

--- public_html/index.php
+++ public_html/index.php
@@ -3,6 +3,11 @@ require '../vendor/autoload.php';
 
 $app = new \Slim\Slim();
 
+
+$app->get('/', function ($name) {
+    echo "wut?!";
+});
+
 $app->get('/hello/:name', function ($name) {
     echo "Hello, $name";
 });
