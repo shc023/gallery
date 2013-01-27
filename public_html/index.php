7ce592d72e8cc0cd8a7442ca4a44d8b15014a69c
Chris Bednarski <banzaimonkey@gmail.com>
2013-01-26 20:58:04 -0800

    Added config.php.template Added config.php to index.php

--- public_html/index.php
+++ public_html/index.php
@@ -1,9 +1,15 @@
 <?php
 
 require_once(__DIR__ . '/../vendor/autoload.php');
+require_once(__DIR__ . '/../configs/config.php');
+
+if ($debug) {
+    error_reporting(E_ALL); // or E_STRICT
+    ini_set("display_errors", 1);
+    ini_set("memory_limit", "1024M");
+}
 
 $app = new \Slim\Slim();
-
 
 $app->get('/', function () use ($app) {
     $app->render('bucket.php');
